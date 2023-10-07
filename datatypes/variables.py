78d365a300b3de7d9e3d9c7d4f0facc7d7736a21
PrasadSaralaya <prasad6saralaya>
2023-10-07 12:31:24 +0530

    Added other class lessons

--- datatypes/variables.py
+++ datatypes/variables.py
@@ -24,14 +24,53 @@ iq = 100  # statement
 
 age = iq / 5  # entire line is a statement while iq/5 is an expression
 
-#augmented assignment variables
+# augmented assignment variables
 
 value = 5
 value = value + 2
-print("value - " + str(value))
+print('value - ' + str(value))
 # above statement can be assigned using += which is an augmented assignment operator which
-# will work the same as above
+# will work the same as above. similarly there are *=, -=, /=
 
 value = 5
 value += 2
-print("value in augmented variable - " + str(value))
+print('value in augmented variable - ' + str(value))
+
+# STRINGS
+# 3 single quotes is used to denote long strings coming in multiple lines
+print(type("any type of string value"))
+
+long_string = ''' 
+this is 
+very long
+string
+'''
+username = 'name'
+password = 'pwd'
+
+print(long_string)
+
+# string concatenation
+f_name = 'Prasad'
+l_name = 'Saralaya'
+full_name = f_name + l_name
+full_name_with_space = f_name + ' ' + l_name
+
+print(full_name)
+print(full_name_with_space)
+
+# Type conversion
+print('type conversion ')
+a = str(100)
+print(type(a))
+# similar to what was done earlier in line number 31, 37
+
+# Escape sequences
+# used in single quote, apostrophe or double quotes
+# whatever comes after backslash is considered as string
+
+weather = 'it\'s \"kind of \" sunny today'
+print(weather)
+# similarly \t \n can be used for tab and newline character
+weather = '\t it\'s \"kind of \" sunny today \n hope you have a good day'
+print(weather)

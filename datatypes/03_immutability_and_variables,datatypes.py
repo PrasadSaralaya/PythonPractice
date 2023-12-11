809b95c692b1c33d55772bf2c6f0dd5cbf8ff6a5
PrasadSaralaya <prasad6saralaya>
2023-12-11 14:31:56 +0530

    Added other class lessons

---
+++ datatypes/03_immutability_and_variables,datatypes.py
@@ -0,0 +1,19 @@
+# Immutability
+
+# strings are immutable in python meaning that the index value string cannot be reassigned
+name = 'prasad'
+print(name)
+name = 'saralaya'
+print(name)  # this is entire re assignment
+# name[1] = 's' # this will error out
+name = name + 'a'
+print(name)  # this wont error out as its addition of a string
+# and entire variable name gets reassigned
+
+# BUILT-IN FUNCTIONS AND METHODS
+print(len('prasad'))
+
+
+
+if (int(float('1.0000'))) == 1:
+    print('prasad')

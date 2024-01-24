12a90b364c6cf97c959a4804612c3ca32e925cb5
PrasadSaralaya <prasad6saralaya>
2024-01-24 21:51:17 +0530

    Added other class lessons

---
+++ datastructures/06_tuples.py
@@ -0,0 +1,13 @@
+# Tuples are like list but are immutable
+# cant sort or reverse the tuple
+# but in case of performance is better than lists
+
+
+basket = (1, 2, 3, 4, 5)
+print(basket)
+print(basket[2])
+print(5 in basket)  # returns true or false like list
+
+# basket[0] = 9 # errors out as tuples are immutable
+# tuple can be used as key value in the dictionary as well
+
---
+++ datastructures/07_sets.py
@@ -0,0 +1,4 @@
+list = [1, 9, 3, 4, 5, 6, 6, 7, 7, 8]
+
+print(list)
+print(set(list))

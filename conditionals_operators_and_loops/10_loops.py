7b9a1a2659ab0f6c92e79c0cfe627be29b177410
PrasadSaralaya <prasad6saralaya>
2024-03-25 15:49:56 +0530

    Added other class lessons

--- conditionals_operators_and_loops/10_loops.py
+++ conditionals_operators_and_loops/10_loops.py
@@ -150,3 +150,24 @@ while True:
 # break breaks the loop be it while or for
 # continue will continue the loop when it reaches a particular line no matter what
 # pass will pass the to the next line
+
+
+# EXERCISE
+
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+for image in picture:
+    for pixel in image:
+        if pixel:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print('')
